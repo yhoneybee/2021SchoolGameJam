c3ffe8ead200a3c6564a60390277b30e580085df
qkrgustn <sdh210212@sdh.hs.kr>
2021-11-27 17:13:41 +0900

    씬 추가 및 메인 -> 인게임 버튼, 스코어 텍스트 넣음

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -6,7 +6,7 @@ public class ScoreText : MonoBehaviour
 {
     public Text scoreText;
     public float KillMob = 0;
-    private void Start()
+    private void FixedUpdate()
     {
         //if(잡을때마다)
         scoreText.text = "목표 점수 = 2022" + "\n현재 점수 : " + KillMob.ToString();
